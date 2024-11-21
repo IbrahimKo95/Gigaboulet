94b2adb1b6877ee40c984fbcdf4fac4d01f7c43c
Hugo <espenanhugo@gmail.com>
2024-11-21 14:55:29 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name("home");
+Route::view('/a-propos', 'about');
 Route::get('/{category}', [CategoryController::class, 'index']);
 Route::get('/article/{slug}', [ArticleController::class, 'index']);
